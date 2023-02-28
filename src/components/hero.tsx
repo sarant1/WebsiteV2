f951c906290c58355147b8189a9b8049960b79e4
sarant1 <samuel.arant10@gmail.com>
2023-02-28 15:23:46 -0500

    made hero look a little better/still needs work

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,38 +1,42 @@
 export const Hero = () => {
   return (
-    <div className="bg-slate-400 m-2 rounded-lg">
-      <div className="flex flex-row justify-between">
-        <img
-          className="h-[300px] rounded-xl shadow-xl ml-4"
-          src={require("../images/grandteton.JPEG")}
-          alt=""
-        />
-        <div className="bg-slate-600 ml-5">
-          <h1 className="">Hi, I am Sam,</h1>
-          <p className="">
-            After previously persuing a career in the medical field, I am
-            switching to tech, specifically the fast growing field of cloud
-            computing. I have a strong background in computer science despite
-            missing a CS degree. Currently, I focus mainly on the AWS platform
-            but open to learning any platform.
-          </p>
+    <div className="bg-slate-400 rounded-lg m-2">
+      <div className="flex flex-row p-4 flex-wrap">
+        <div className="self-center">
+          <img
+            className="rounded-xl shadow-xl w-[380px]"
+            src={require("../images/grandteton.JPEG")}
+            alt=""
+          />
         </div>
 
-        <div className="pt-4 pb-4">
-          <div className="w-[15vw]">
-            <img
-              className="hidden 2xl:block min-w-[150px] p-2"
-              src={require("../icons/awsdeveloper.png")}
-              alt=""
-            />
+        <div className="flex-1 ml-4 flex flex-col flex-wrap justify-between rounded-lg min-w-[333px]">
+          <div className="p-8 mb-4 text flex-1">
+            <h1 className="text-3xl mb-4">Hello I'm Sam,</h1>
+            <p className="text-xl">
+              After previously persuing a career in the medical field, I am
+              switching to tech, specifically the fast growing field of cloud
+              computing. I have a strong background in computer science despite
+              missing a CS degree. Currently, I focus mainly on the AWS platform
+              but open to learning any platform.
+            </p>
           </div>
+          <div className="pt-4 pb-4 flex flex-row justify-start h-[200px]">
+            <div className="">
+              <img
+                className="hidden 2xl:block min-w-[150px] h-full"
+                src={require("../icons/awsdeveloper.png")}
+                alt=""
+              />
+            </div>
 
-          <div className="w-[15vw]">
-            <img
-              className="hidden 2xl:block min-w-[150px] p-2"
-              src={require("../icons/awspractioner.png")}
-              alt=""
-            />
+            <div className="">
+              <img
+                className="hidden 2xl:block min-w-[150px] h-full"
+                src={require("../icons/awspractioner.png")}
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </div>
